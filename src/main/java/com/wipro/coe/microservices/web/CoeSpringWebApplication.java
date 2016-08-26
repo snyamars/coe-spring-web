f63de5e365890dea9afb2d8cd8a75d6596e075af
root <ranajit.jana@wipro.com>
2016-08-26 22:54:44 +0530

    initial checkin

---
+++ src/main/java/com/wipro/coe/microservices/web/CoeSpringWebApplication.java
@@ -0,0 +1,12 @@
+package com.wipro.coe.microservices.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoeSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoeSpringWebApplication.class, args);
+	}
+}
