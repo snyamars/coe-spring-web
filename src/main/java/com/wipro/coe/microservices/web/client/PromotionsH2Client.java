50d857f62e294b3ee1f94a661e5da66f96d310dd
ranajit-jana <ranajit.jana@wipro.com>
2018-04-14 16:46:52 +0530

    Client for H2 db added

---
+++ src/main/java/com/wipro/coe/microservices/web/client/PromotionsH2Client.java
@@ -0,0 +1,30 @@
+package com.wipro.coe.microservices.web.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.wipro.coe.microservices.web.domain.Promotion;
+
+@FeignClient("coe-h2-promotions")
+public interface PromotionsH2Client {
+	
+	@RequestMapping(value = "/promotions", method = RequestMethod.GET)
+	Resources<Resource<Promotion>>  getAllPromotions();
+	
+	
+	@RequestMapping(value = "/promotions/{id}", method = RequestMethod.GET)
+	Resource<Promotion> get(@PathVariable("id") Long id);
+	
+	@RequestMapping(value = "/promotions", method = RequestMethod.POST)
+	Resource<Promotion> save(Promotion Promotion);
+	
+	@RequestMapping(value = "promotions/{id}" ,method = RequestMethod.PUT) 
+	Resource<Promotion> update(@PathVariable("id") long id, Promotion promotion);
+
+	@RequestMapping(value = "promotions/{id}"  , method = RequestMethod.DELETE)  
+	void delete(@PathVariable("id") long id);
+}	
